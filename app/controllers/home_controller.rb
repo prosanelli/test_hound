a2ca085176ecb2f9d83071cd27e0039435100c47
prosanelli <prosanelli@weddingwire.com>
2015-04-02 11:18:15 -0400

    test 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,24 @@ class HomeController < ApplicationController
 
     @message = "Hello World"
 
-    numbers = [ 1, 2, 3]
+    @numbers = [ 1, 2, 3]
 
     num = 1000000
 
+
+    @math = Math.sqrt( 1 + 2 )
+
+    if(1 > 5)
+      @test1 = 'nope'
+    end
+
+    @x = []
+    [1, 2, 3].each do |n|
+      @x.push n
+    end
+
+
+    @p = Proc.new { |n| puts n }
 
   end
 
